48a013eb1efcdbdfb53965a2b87373e8861bb112
Shrutesh Thorve <thorveshrutesh@gmail.com>
2024-06-12 19:10:41 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', // Include all JavaScript and TypeScript files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
